e29981591c2c19ad4d2885833ea28ea7b9acc3c9
kennythink <kenny.think@gmail.com>
2016-09-30 15:27:22 +0800

    add Switch test

---
+++ SwitchTest/app/src/main/java/com/example/admin/switchtest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.admin.switchtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://developer.android.com/reference/android/widget/Switch.html
+ * http://stackoverflow.com/questions/10173590/how-to-change-the-size-of-a-switch-widget
+ * http://blog.csdn.net/u012585142/article/details/50756872
+  */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
